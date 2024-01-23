842d5ada630b00f1f5f12e8dd4e72b99c047b1ef
Julian <julian@wheresnick.com.au>
2024-01-23 13:16:29 +1100

    minor tweaks

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,17 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
-
+import { ApolloProvider } from '@apollo/client';
+import client from './apolloClient';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    <ApolloProvider client={client}> 
+      <>
+        <Navbar />
+        <Outlet />
+      </>
+    </ApolloProvider>
   );
 }
 
--- client/src/pages/SavedBooks.jsx
+++ client/src/pages/SavedBooks.jsx
@@ -1,9 +1,7 @@
-import { useState } from 'react';
-import { Container, Card, Button, Row, Col } from 'react-bootstrap';
 import { useQuery, useMutation } from '@apollo/client';
 import { GET_ME } from '../graphql/queries';
 import { REMOVE_BOOK } from '../graphql/mutations';
-
+import { Container, Card, Button, Row, Col } from 'react-bootstrap';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
@@ -13,7 +11,7 @@ const SavedBooks = () => {
     update(cache, { data: { removeBook } }) {
       cache.modify({
         fields: {
-          me(existingMe = []) {
+          me(existingMeData) {
             return removeBook;
           },
         },
@@ -41,37 +39,26 @@ const SavedBooks = () => {
   const userData = data?.me;
 
   return (
-    <>
-      <div fluid className="text-light bg-dark p-5">
-        <Container>
-          <h1>Viewing saved books!</h1>
-        </Container>
-      </div>
-      <Container>
-        <h2 className='pt-5'>
-          {userData.savedBooks.length
-            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
-            : 'You have no saved books!'}
-        </h2>
-        <Row>
-          {userData.savedBooks.map((book) => (
-            <Col md="4" key={book.bookId}>
-              <Card border='dark'>
-                {book.image && <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' />}
-                <Card.Body>
-                  <Card.Title>{book.title}</Card.Title>
-                  <p className='small'>Authors: {book.authors}</p>
-                  <Card.Text>{book.description}</Card.Text>
-                  <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
-                    Delete this Book!
-                  </Button>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
-        </Row>
-      </Container>
-    </>
+    <Container fluid className="text-light bg-dark p-5">
+      <h1>Viewing saved books!</h1>
+      <Row>
+        {userData.savedBooks.map((book) => (
+          <Col md="4" key={book.bookId}>
+            <Card border='dark'>
+              {book.image && <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' />}
+              <Card.Body>
+                <Card.Title>{book.title}</Card.Title>
+                <p className='small'>Authors: {book.authors.join(', ')}</p>
+                <Card.Text>{book.description}</Card.Text>
+                <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
+                  Delete this Book!
+                </Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 };
 
