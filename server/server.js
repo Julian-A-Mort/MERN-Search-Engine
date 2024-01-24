729bc4826f2333eb06573e096d962d0b2156db19
Julian <julian@wheresnick.com.au>
2024-01-25 10:08:41 +1100

    more server changes

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ const startApolloServer = async () => {
   }));
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../client/dist')));
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../client/build/index.html'));
     });
@@ -47,4 +47,4 @@ const startApolloServer = async () => {
   });
 };
 
-startApolloServer();
+startApolloServer(); 
